f54eace943ee1c5c8a33f9af9ed2d7c1e99487e4
Depray162 <fabricio1quesada@gmail.com>
2024-06-08 18:45:22 -0600

    middleware

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -10,22 +10,22 @@ use App\Http\Middleware\ApiAuthMiddlewareCliente;
 
     Route::prefix('v1')->group(
         function () {
-            Route::resource('/cliente', ClienteController::class, ['except' => ['create', 'edit']]);
-            Route::resource('/telefono', TelefonoController::class, ['except' => ['create', 'edit']]);
-            Route::resource('/vehiculo', VehiculoController::class, ['except' => ['create', 'edit']]);
-            Route::resource('/recorrido', RecorridoController::class, ['except' => ['create', 'edit']]);
-            Route::resource('/destino', DestinoController::class, ['except' => ['create', 'edit']]);
-
+            
           
             
             Route::group(['prefix' => '/cliente'], function () {
                   Route::post('/loginCli', [ClienteController::class, 'loginCli'])->withoutMiddleware(ApiAuthMiddlewareCliente::class);
-                  Route::post('/editarDatos', [ClienteController::class, 'update'])->withoutMiddleware(ApiAuthMiddlewareCliente::class);
+                  Route::get('/verinfo/{id}', [ClienteController::class, 'show'])->middleware(ApiAuthMiddlewareCliente::class);
 
                 
             });
     
 
-
+/*Route::resource('/cliente', ClienteController::class, ['except' => ['create', 'edit']]);
+            Route::resource('/telefono', TelefonoController::class, ['except' => ['create', 'edit']]);
+            Route::resource('/vehiculo', VehiculoController::class, ['except' => ['create', 'edit']]);
+            Route::resource('/recorrido', RecorridoController::class, ['except' => ['create', 'edit']]);
+            Route::resource('/destino', DestinoController::class, ['except' => ['create', 'edit']]);
+*/
         }
     );
